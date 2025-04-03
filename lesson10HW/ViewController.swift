9a3d13b3808ced1cd91eff0d042cd7cccd3e96c6
PetrushenkoDmitry <petrushenkodmitrii@yandex.ru>
2025-04-03 12:37:11 +0300

    Initial Commit

---
+++ lesson10HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson10HW
+//
+//  Created by Дмитрий Петрушенко on 03/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
